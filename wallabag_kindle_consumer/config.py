526817de612bbf96f3118c5e0314450b2f05a774
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-03-21 12:58:02 +0100

    Add basic config handling
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- wallabag_kindle_consumer/config.py
+++ wallabag_kindle_consumer/config.py
@@ -1,6 +1,11 @@
 class Config:
 
-    def __init__(self, db_uri, refresh_grace=120, consume_interval=30):
+    def __init__(self, wallabag_host, db_uri, tag='kindle', refresh_grace=120, consume_interval=30,
+                 interface_host="127.0.0.1", interface_port=8080):
+        self.wallabag_host = wallabag_host
         self.db_uri = db_uri
+        self.tag = tag
         self.refresh_grace = refresh_grace
         self.consume_interval = consume_interval
+        self.interface_host = interface_host
+        self.interface_port = interface_port
